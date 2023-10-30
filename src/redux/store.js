5ebabd78431ffe318365a5b5d5909e8c81a045f1
raihanbadruz <raihanzaman79@gmail.com>
2023-10-30 22:10:15 +0700

    Done Challange

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,6 @@ import rootReducers from "./reducers";
 // Create the store
 export default configureStore({
   reducer: rootReducers,
-  devTools: import.meta.env.NODE_ENV === "development",
+  devTools: import.meta.env.DEV,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // It not necessary if your feature is not too complex, you can just comment this line if you don't need it
 });
